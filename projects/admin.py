8919fe0e806b57bfc920236f2a41b34d2e3d445a
BA <engineervinceblair@gmail.com>
2020-06-07 23:54:49 +0300

    registered models,post and profile on admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Post, Profile
+admin.site.site_header=' Blaircarson Software Engineer'
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Profile)
