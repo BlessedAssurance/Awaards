32bc088d4a31a8e90586a3100875e10c1cc860da
BA <engineervinceblair@gmail.com>
2020-06-07 22:14:47 +0300

    created test method for saving post

--- projects/tests.py
+++ projects/tests.py
@@ -7,9 +7,9 @@ class ProfileTestClass(TestCase):
     #Set up method
 
     def setUp(self):
-        self.new_user = User(username='BettySeron', email='chepkiruibetty64@gmail.com', password='1234')
+        self.new_user = User(username='BLAIRCARSON', email='engineervinceblair@gmail.com', password='29484510c')
         self.new_user.save()
-        self.new_profile = Profile(user=self.new_user,profile_picture="image.jpeg",bio="Hello there!", contact='chepkiruibetty64@gmail.com')
+        self.new_profile = Profile(user=self.new_user,profile_picture="image.jpeg",bio="Hello there!", contact='engineervinceblair@gmail.com')
     
     # Testing  instance
     def test_instance(self):
@@ -25,3 +25,24 @@ class ProfileTestClass(TestCase):
         self.new_profile.delete_profile()
         profile = Profile.objects.all()
         self.assertTrue(len(profile)==0)   
+
+class PostTestClass(TestCase):
+
+    def setUp(self):
+        
+        self.new_user = User(username='BLAIRCARSON', email='engineervinceblair@gmail.com', password='29484510c')
+        self.new_user.save()
+        self.new_post=Post(title="Blog",image='image.png',description="app that enables users to post their thoughts",user=self.new_user,link="https://blaircarsonblog.herokuapp.com/",country="kenya")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_post,Post))    
+
+    def test_save_post(self):
+        self.new_post.save_post()
+        post = Post.objects.all()
+        self.assertTrue(len(post)>0)
+
+    def test_delete_post(self):
+        self.new_post.save_post()
+        self.new_post.delete_post()
+        post = Post.objects.all()
