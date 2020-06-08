3938c20203eac62ad038e9eb77090199fd9e1fff
BA <engineervinceblair@gmail.com>
2020-06-09 00:26:46 +0300

    registered user in the admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username',]
+
+admin.site.register(CustomUser, CustomUserAdmin)
